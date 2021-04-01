459c714501ef3f36819c262db25dad287ad10dc3
Andressa Bovolenta <andressa.bbovolenta@gmail.com>
2021-03-31 21:00:47 -0300

    First commit

---
+++ Aluno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_class
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public string Senha { private get; set; }
+        public string Curso { get; set; }
+        public string Disciplina { get; set; }
+
+        public double Nota = 0;
+        private double Media = 0;
+
+        public Aluno(double media)
+        {
+            Media = media;
+        }
+
+        public void InserirNota(double nota)
+        {
+            Nota = Nota + nota;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            int provas = 4;
+            double notaFinal = Nota / 4;
+            return Media + notaFinal;
+        }
+
+        public void ImprimirMedia()
+        {
+            Console.WriteLine($"{Nome}, sua média para a disciplina '{Disciplina}' do curso '{Curso}' é {CalcularNotaFinal()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace student_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno(0);
+
+            aluno.Nome = "Andressa Bovolenta";
+            aluno.Matricula = "7513491";
+            aluno.Curso = "Tecnologia da Informação";
+            aluno.Disciplina = "Programação Orientada a Objetos";
+                                 
+            aluno.InserirNota(9.4);                        
+            aluno.InserirNota(8.6);            
+            aluno.InserirNota(9.2);                        
+            aluno.InserirNota(7.8);
+
+            aluno.ImprimirMedia();
+        }
+    }
+}
